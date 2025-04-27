9533370dac6f361ee57fe3f9e17edcfc242dae11
Dhirajsingh212 <dhirajsingh0229@gmail.com>
2025-04-28 02:49:01 +0530

    ui updates

--- client/src/components/BlogCard.tsx
+++ client/src/components/BlogCard.tsx
@@ -1,33 +1,47 @@
+import { BASE_URL } from "@/config/colors";
+import { useUserStore } from "@/store/store";
+import axios from "axios";
 import { Dot } from "lucide-react";
+import { useEffect, useState } from "react";
+
+interface Blogs {
+  id: string;
+  title: string;
+  content: string;
+  createdAt: string;
+}
 
 export function BlogCard() {
-  const imageGroups = [
-    "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg",
-    "https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg",
-  ];
+  const userState = useUserStore();
+  const [allBlogs, setAllBlogs] = useState<Blogs[]>([]);
+
+  useEffect(() => {
+    const fetchBlogs = async () => {
+      const response = await axios.get(`${BASE_URL}/blog/getAll`, {
+        headers: {
+          Authorization: `Bearer ${userState.jwtToken}`,
+        },
+      });
+      setAllBlogs(response.data);
+    };
+    fetchBlogs();
+  }, []);
 
   return (
     <div className="columns-1 sm:columns-2 lg:columns-3 xl:columns-4 gap-4 space-y-4">
-      {imageGroups.map((src, imgIndex) => (
+      {allBlogs.map((src, imgIndex) => (
         <div
           className="break-inside-avoid overflow-hidden rounded-lg border-2 border-violet-950 shadow-2xl"
           key={imgIndex}
         >
-          <img className="w-full h-auto rounded-t-lg" src={src} alt="" />
+          <img
+            className="w-full h-auto rounded-t-lg"
+            src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg"
+            alt=""
+          />
           <div className="px-4 pb-4">
             <p className="text-xl font-semibold pt-4 hover:text-violet-700 transition-all cursor-pointer duration-300">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam,
-              asperiores!
+              {src.title}
             </p>
             <p className="flex flex-row items-center text-muted-foreground pt-4">
               <img
